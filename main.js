35fa0224c073ec3e9ae12553a429bc297ac3d6e7
Gaspare <Gas9220@users.noreply.github.com>
2023-03-15 18:51:41 +0100

    Aggiunto il supporto multilista

--- main.js
+++ main.js
@@ -3,26 +3,34 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            todoList: [
-                {
-                    text: "Andare in pizzeria",
-                    isCompleted: false
-                },
-                {
-                    text: "Fare la spesa",
-                    isCompleted: true
-                },
-                {
-                    text: "Seguire la lezione",
-                    isCompleted: false
-                },
+            todoLists: [
+                todoList = {
+                    name: "Default",
+                    elements:
+                        [
+                            {
+                                text: "Andare in pizzeria",
+                                isCompleted: false
+                            },
+                            {
+                                text: "Fare la spesa",
+                                isCompleted: true
+                            },
+                            {
+                                text: "Seguire la lezione",
+                                isCompleted: false
+                            }
+                        ]
+                }
             ],
-            newToDoText: ''
+            newToDoText: '',
+            currentTodoList: 0,
+            newListName: ''
         }
     },
     methods: {
         removeTodo(index) {
-            this.todoList.splice(index, 1) 
+            this.todoLists[this.currentTodoList].elements.splice(index, 1)
         },
         addNewTodo() {
             if (this.newToDoText.trim() !== '') {
@@ -31,13 +39,32 @@ createApp({
                     isCompleted: false
                 }
 
-                this.todoList.push(newTodo)
+                this.todoLists[this.currentTodoList].elements.push(newTodo)
 
                 this.newToDoText = ''
-            }  
+            }
         },
-         toggleTodo(todo) {
+        toggleTodo(todo) {
             return todo.isCompleted ? todo.isCompleted = false : todo.isCompleted = true;
+        },
+        createNewList() {
+            const newList = {
+                name: this.newListName,
+                elements:
+                    []
+            }
+
+            this.todoLists.push(newList)
+
+            this.newListName = ''
+        },
+        changeList(index) {
+            this.currentTodoList = index
+        },
+        isListSelected(list) {
+            if (this.todoLists[this.currentTodoList] === list) {
+                return "selected-list"
+            }
         }
     }
 }).mount('#app')
